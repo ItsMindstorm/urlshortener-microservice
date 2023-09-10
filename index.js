ad0bb9670b3ca484383b159c0fe352a1903be13e
yvess <yvess.straten@gmail.com>
2023-09-10 10:32:18 +0800

    Added localStorage and added this store to gitignore

--- index.js
+++ index.js
@@ -3,105 +3,109 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const parser = require("body-parser");
+const store = require("node-localstorage")
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+const storage = store.LocalStorage
+localStorage = new storage("./codeurl")
+let codeUrl = [];
+
+if (localStorage.getItem("urldata") === null) {
+	console.log("Empty")
+} else {
+	codeUrl = JSON.parse(localStorage.getItem("urldata"))
+	console.log(codeUrl)
+}
 
 app.use(cors());
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+app.get("/", function(req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
 });
 
 app.use(
-  parser.urlencoded({
-    extended: false,
-  }),
+	parser.urlencoded({
+		extended: false,
+	}),
 );
 
-let codeUrl = [];
 
 const makeShortened = () => {
-  // Makes a shortened, 5 random characters code
-  const randValues = new Uint16Array(1);
-  const shortUrl = crypto.getRandomValues(randValues);
+	// Makes a shortened, 5 random characters code
+	const randValues = new Uint16Array(1);
+	const shortUrl = crypto.getRandomValues(randValues);
 
-  const finalShort = shortUrl[0];
+	const finalShort = shortUrl[0];
 
-  return finalShort;
+	return finalShort;
 };
 
-app.post("/api/shorturl", function (req, res) {
-  // This gets the host name from the input url
-  try {
-    const inputUrl = new URL(req.body.url);
+app.post("/api/shorturl", function(req, res) {
+	// This gets the host name from the input url
+	try {
+		const inputUrl = new URL(req.body.url);
+		const hostname = inputUrl.hostname;
+		console.log(hostname);
 
-    const exists = urlExists(inputUrl);
-
-    if (exists === false) {
-      res.json({
-        error: "invalid host",
-      });
-    }
-
-    /* Calls the function, getting the shortened code
-     */
-    const shortened = makeShortened();
-    // Returns URL and shortened code as json
-    res.json({
-      original_url: req.body.url,
-      short_url: shortened,
-    });
-    // Pushes url and code into the same array, one after another
-    codeUrl.push(req.body.url);
-    codeUrl.push(shortened);
-    console.log(codeUrl);
-  } catch ({ name, message }) {
-    console.log(name);
-    console.log(message);
-    res.json({
-      error: "invalid url",
-    });
-  }
+		/* Calls the function, getting the shortened code
+		 */
+		const shortened = makeShortened();
+		// Returns URL and shortened code as json
+		res.json({
+			original_url: req.body.url,
+			short_url: shortened,
+		});
+		// Pushes url and code into the same array, one after another
+		codeUrl.push(req.body.url);
+		codeUrl.push(shortened);
+		console.log(codeUrl);
+		localStorage.setItem("urldata", JSON.stringify(codeUrl))
+	} catch (error) {
+		res.json({
+			error: "invalid url",
+		});
+	}
 });
 
-app.get("/api/shorturl/:url", function (req, res) {
-  /* Function finds whether the provided code matches the same code in the array */
-  const matchingShort = () => {
-    const matching = codeUrl.find((code) => {
-      return code.toString() === req.params.url;
-    });
-    console.log(codeUrl.indexOf(matching));
-    return matching;
-  };
+app.get("/api/shorturl/:url", function(req, res) {
+	/* Function finds whether the provided code matches the same code in the array */
+	const matchingShort = () => {
+		const matching = codeUrl.find((code) => {
+			return code.toString() === req.params.url;
+		});
+		console.log(codeUrl.indexOf(matching));
+		return matching;
+	};
 
-  const matching = matchingShort();
+	const matching = matchingShort();
 
-  /* Function grabs the url in the array that is 1 position before the index of the provided code */
-  const matchingUrl = () => {
-    const url = codeUrl[codeUrl.indexOf(matching) - 1];
-    console.log(url);
+	/* Function grabs the url in the array that is 1 position before the index of the provided code */
+	const matchingUrl = () => {
+		const url = codeUrl[codeUrl.indexOf(matching) - 1];
+		console.log(url);
 
-    return url;
-  };
+		return url;
+	};
 
-  const url = matchingUrl();
+	const url = matchingUrl();
 
-  /* Checks whether there is a match provided by the function, if there is, it will redirect the user to the matched url from the function */
-  if (matching) {
-    console.log("if statement working!");
-    // Redirects to original url
-    res.redirect(url);
-  } else {
-    // Throws an error if the code is invalid
-    res.json({
-      error: "invalid url",
-    });
-  }
+	/* Checks whether there is a match provided by the function, if there is, it will redirect the user to the matched url from the function */
+	if (matching) {
+		console.log("if statement working!");
+		// Redirects to original url
+		res.redirect(url);
+	} else {
+		// Throws an error if the code is invalid
+		res.json({
+			error: "invalid url",
+		});
+	}
 });
 
-app.listen(port, function () {
-  console.log(`Listening on port ${port}`);
+app.listen(port, function() {
+	console.log(`Listening on port ${port}`);
 });
