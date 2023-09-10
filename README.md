97aa731d9032d241d60120aa58d4e6cdd13213a9
yvess <yvess.straten@gmail.com>
2023-09-10 14:35:09 +0800

    Fully switched to mongoDB

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # URL Shortener Microservice
+This microservice turns a valid input url of the format: `https://www.youraddress.com` into a shortened number code in the form of JSON: 
+```json 
+{
+    "original_url": "Your input url",
+    "short_url": 32460
+}
+```
+This code can then used with a `GET` request at `/api/shorturl/:url` and the service will redirect you to your desired origin url. 
 
-This is the boilerplate code for the URL Shortener Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice.
+The codes and associated urls are stored in a mongoDB database in the cloud.
--- index.js
+++ index.js
@@ -4,20 +4,13 @@ const cors = require("cors");
 const app = express();
 const parser = require("body-parser");
 const store = require("node-localstorage")
+const { MongoClient } = require("mongodb")
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
-const storage = store.LocalStorage
-localStorage = new storage("./codeurl")
-let codeUrl = [];
-
-if (localStorage.getItem("urldata") === null) {
-	console.log("Empty")
-} else {
-	codeUrl = JSON.parse(localStorage.getItem("urldata"))
-	console.log(codeUrl)
-}
+const client = new MongoClient(process.env.DB_URL)
+const db = client.db("shorturl")
+const urls = db.collection("shorturl")
 
 app.use(cors());
 
@@ -59,11 +52,8 @@ app.post("/api/shorturl", function(req, res) {
 			original_url: req.body.url,
 			short_url: shortened,
 		});
-		// Pushes url and code into the same array, one after another
-		codeUrl.push(req.body.url);
-		codeUrl.push(shortened);
-		console.log(codeUrl);
-		localStorage.setItem("urldata", JSON.stringify(codeUrl))
+		// Pushes url and code to db
+		urls.insertOne({ original_url: req.body.url, short_url: shortened })
 	} catch (error) {
 		res.json({
 			error: "invalid url",
@@ -71,39 +61,13 @@ app.post("/api/shorturl", function(req, res) {
 	}
 });
 
-app.get("/api/shorturl/:url", function(req, res) {
-	/* Function finds whether the provided code matches the same code in the array */
-	const matchingShort = () => {
-		const matching = codeUrl.find((code) => {
-			return code.toString() === req.params.url;
-		});
-		console.log(codeUrl.indexOf(matching));
-		return matching;
-	};
-
-	const matching = matchingShort();
-
-	/* Function grabs the url in the array that is 1 position before the index of the provided code */
-	const matchingUrl = () => {
-		const url = codeUrl[codeUrl.indexOf(matching) - 1];
-		console.log(url);
-
-		return url;
-	};
-
-	const url = matchingUrl();
-
-	/* Checks whether there is a match provided by the function, if there is, it will redirect the user to the matched url from the function */
-	if (matching) {
-		console.log("if statement working!");
-		// Redirects to original url
-		res.redirect(url);
-	} else {
-		// Throws an error if the code is invalid
-		res.json({
-			error: "invalid url",
-		});
-	}
+app.get("/api/shorturl/:url", async (req, res) => {
+	console.log(req.params.url)
+	const redirect = await urls.findOne({
+		short_url: parseInt(req.params.url)
+	});
+	console.log(redirect)
+	res.redirect(redirect.original_url)
 });
 
 app.listen(port, function() {
